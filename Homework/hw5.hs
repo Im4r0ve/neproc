a971998f3f91c8972da263edd50e359b0e16121a
Im4r0ve <dominikjelinek98@gmail.com>
2020-05-01 08:26:23 +0200

    insertwith pred pridanim dalsieho prvku ktory poznam

--- Homework/hw5.hs
+++ Homework/hw5.hs
@@ -3,20 +3,28 @@
 -- 1) Definujte datový typ 'Trie k v' reprezentující trii, kde klíče (řetězce)
 -- jsou typu '[k]' a hodnoty typu 'v'.
 
-data Trie k v = TODO
+data Trie k v = Node [(k, Trie k v)] (Maybe v) 
+    deriving (Show,Eq)
 
 -- Implementujte následující:
 
 empty :: Trie k v
-empty = undefined
+empty = Node [] Nothing
 
--- 'empty' je jednoduše konstanta, reprezentující prádznou trii.
 --
 -- > empty == fromList []
 --
-
+{-
+insert :: Int -> IntTree -> IntTree
+insert x IntLeaf = IntNode x IntLeaf IntLeaf
+insert x (IntNode y l r)
+    | x <  y    = IntNode y (insert x l) r
+    | x == y    = IntNode x l r
+    | otherwise = IntNode y l (insert x r)
+-}
 singleton :: [k] -> v -> Trie k v
-singleton = undefined
+singleton [] v = Node [] (Just v)
+singleton (x:xs) v = Node [(x,singleton xs v)] Nothing 
 
 -- 'singleton ks v' je trie, která obsahuje právě jednen klíč 'ks'
 -- s hodnotou 'v'.
@@ -24,9 +32,19 @@ singleton = undefined
 -- > singleton ks v == fromList [(ks, v)]
 --
 
-insertWith :: (Ord k) => (v -> v -> v) -> [k] -> v -> Trie k v -> Trie k v
-insertWith = undefined
 
+insertWith :: (Ord k) => (v -> v -> v) -> [k] -> v -> Trie k v -> Trie k v
+insertWith (f) [] new_v (Node [_] Nothing) = Node [] (Just new_v) 
+insertWith (f) [] new_v (Node [_] (Just old_v)) = Node [] (Just (f new_v old_v)) 
+--insertWith (f) (x:xs) new_v (Node sub_trees old_v) =
+ --   case (lookup x sub_trees) of
+  --      Just sub_tree   -> insertWith (f) xs new_v sub_tree
+    --    Nothing         -> Node ((x,singleton xs new_v):sub_trees) old_v
+
+insertWith (f) (x:xs) new_v (Node [] old_v) = Node [(x,singleton xs new_v)] old_v
+insertWith (f) (x:xs) new_v (Node ((k,sub_trie):ns) old_v)
+    | x == k    = Node ((k,insertWith (f) xs new_v sub_trie):ns) old_v
+    | otherwise = Node ((k,sub_trie):(x,insertWith (f) (x:xs) new_v ns)) old_v
 -- 'insertWith f ks new t' vloží klíč 'ks' s hodnotou 'new' do trie 't'. Pokud
 -- trie již klíč 'ks' (s hodnotou 'old') obsahuje, původní hodnota je nahrazena
 -- hodnotou 'f new old'.
